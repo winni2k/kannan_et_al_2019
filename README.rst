26c39484ac61abadacab4a5195886b40442cb92b
Warren Kretzschmar <winni@warrenwk.com>
2019-01-31 15:56:25 +0100

    Clean up workflow

--- README.rst
+++ README.rst
@@ -4,3 +4,11 @@ kannan_et_al_2019
 RNA-seq analyses for [kannan2019]_.
 
 .. [kannan2019] Kannan, et al. (2019) Radiation resistant cancer cells enhance the survival and resistance of sensitive cells in prostate spheroids  Cell interactions paper RNA-seq analyses
+
+Setup
+-----
+
+- Install `conda <https://conda.io/en/latest/miniconda.html>`_ and activate an environment.
+- Run ``bash setup.sh``
+
+
--- setup.sh
+++ setup.sh
@@ -1,31 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
-set +u
-. ./activate.sh
-set -u
 
-conda install -y --file requirements.txt -c conda-forge -c bioconda -c anaconda || \
-    conda install -y --file requirements.txt -c conda-forge -c bioconda -c anaconda
-
-pip install git+git://github.com/kvg/dmpy.git
-
-. activate rna_seq_py27
-pip install --requirement requirements-py27.txt
-. activate rna_seq
+conda env create -vv -f environment.yml
+conda env create -vv -f environment_py27.yml
+conda activate rna_seq
 
 # manually download gatk and register it
 mkdir -p opt
 if [ ! -f opt/*/GenomeAnalysisTK.jar ]; then
-    curl https://software.broadinstitute.org/gatk/download/auth?package=GATK | tar -xjf - -C opt
+    curl 'https://software.broadinstitute.org/gatk/download/auth?package=GATK' | tar -xjf - -C opt
 fi
 gatk-register opt/*/GenomeAnalysisTK.jar
 
-
 # link data
-pushd data
-for path in /data3/wkkg/pavitra/*
-do
-    test -L $(basename $path) || -ln -s $path
-done
+# pushd data
+# for path in /data3/wkkg/pavitra/*
+# do
+#     test -L $(basename $path) || -ln -s $path
+# done
 
 
